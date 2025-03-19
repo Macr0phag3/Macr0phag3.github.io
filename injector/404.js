cd7f1f68576f17e3ac126e7c3ce1038a88f3807c
Macr0phag3 <macr0phag3@qq.com>
2025-03-19 11:45:40 +0000

    Site updated: 2025-03-19 11:45:39

---
+++ injector/404.js
@@ -0,0 +1,8 @@
+var autoplay = true;
+var loop = false;
+var songs = {
+	name: '死机之歌',
+	artist: 'DJ Titon',
+	url: 'https://blog-theme-source-1252075454.cos.ap-nanjing.myqcloud.com/bgm/404.mp3',
+	cover: 'https://blog-theme-source-1252075454.cos.ap-nanjing.myqcloud.com/img/bgm-404.jpg'
+}
